955aee0508d991ba0478287b36426b948279545f
srikanthBezawada <srikanth.5283@gmail.com>
2016-01-09 02:05:59 +0530

    invoking jgraphT done, yet to debug

--- IsomorphismInspector/src/main/java/org/cytoscape/isomorphismInspector/internal/logic/IsoThread.java
+++ IsomorphismInspector/src/main/java/org/cytoscape/isomorphismInspector/internal/logic/IsoThread.java
@@ -1,7 +1,15 @@
 package org.cytoscape.isomorphismInspector.internal.logic;
 
+import java.util.List;
 import org.cytoscape.isomorphismInspector.internal.IsoUI;
+import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.experimental.isomorphism.AdaptiveIsomorphismInspectorFactory;
+import org.jgrapht.experimental.isomorphism.GraphIsomorphismInspector;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.SimpleGraph;
 
 /**
  * @author SrikanthB
@@ -21,11 +29,45 @@ public class IsoThread extends Thread{
    
     public void run(){
         menu.startComputation();
+        DefaultDirectedGraph<CyNode, CyEdge> g1 = new DefaultDirectedGraph<CyNode, CyEdge>(CyEdge.class);//SimpleGraph<CyNode, CyEdge>(CyEdge.class)
+        /*
+          1) Change simple graph so that you need not remove self-loops later      
+          2) Edge type <Object> to <CyEdge>, and edge factory fails
+        */        
+        List<CyNode> nodeList1 = network1.getNodeList();
+        List<CyEdge> edgeList1 = network1.getEdgeList();
+        for(CyNode n : nodeList1){
+            g1.addVertex(n);
+        }
+        for(CyEdge e : edgeList1){
+            if(e.getSource().equals(e.getTarget())){
+                continue; // removing self-loops
+            }
+            g1.addEdge(e.getSource(), e.getTarget());
+        }
         
+        UndirectedGraph<CyNode, CyEdge> g2 = new SimpleGraph<CyNode, CyEdge>(CyEdge.class);
+        List<CyNode> nodeList2 = network1.getNodeList();
+        List<CyEdge> edgeList2 = network1.getEdgeList();
+        for(CyNode n : nodeList2){
+            g2.addVertex(n);
+        }
+        for(CyEdge e : edgeList2){
+            if(e.getSource().equals(e.getTarget())){
+                continue; // removing self-loops
+            }
+            g2.addEdge(e.getSource(), e.getTarget());
+        }
         
-        
-        
-        
+        //GraphIsomorphismInspector iso = AdaptiveIsomorphismInspectorFactory.createIsomorphismInspector(g1, g2, null, null);// dafault comparators
+        GraphIsomorphismInspector iso = AdaptiveIsomorphismInspectorFactory.createIsomorphismInspector(g1, g2, new NodeLabelEquivalenceComparator(), new EdgeLabelEquivalenceComparator());
+        boolean isoResult = iso.isIsomorphic();
+  
+        if (isoResult) {
+            System.out.println("Graphs are isomorphic.");
+        } else {
+            System.out.println("Graphs are NOT isomorphic.");
+        }
         menu.endComputation();
     }
     
