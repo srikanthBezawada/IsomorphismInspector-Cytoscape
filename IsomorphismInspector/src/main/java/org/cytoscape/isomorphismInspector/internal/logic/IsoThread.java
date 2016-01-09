147c0b45d63edad8a73fe39570e6a2b9c147a595
smd-faizan <smd.faizan@gmail.com>
2016-01-10 01:07:25 +0530

    minor changes to the way edges are added

--- IsomorphismInspector/src/main/java/org/cytoscape/isomorphismInspector/internal/logic/IsoThread.java
+++ IsomorphismInspector/src/main/java/org/cytoscape/isomorphismInspector/internal/logic/IsoThread.java
@@ -29,7 +29,8 @@ public class IsoThread extends Thread{
    
     public void run(){
         menu.startComputation();
-        DefaultDirectedGraph<CyNode, CyEdge> g1 = new DefaultDirectedGraph<CyNode, CyEdge>(CyEdge.class);//SimpleGraph<CyNode, CyEdge>(CyEdge.class)
+//        DefaultDirectedGraph<CyNode, CyEdge> g1 = new DefaultDirectedGraph<CyNode, CyEdge>(CyEdge.class);//SimpleGraph<CyNode, CyEdge>(CyEdge.class)
+        UndirectedGraph<CyNode, CyEdge> g1 = new SimpleGraph<CyNode, CyEdge>(CyEdge.class);//SimpleGraph<CyNode, CyEdge>(CyEdge.class)
         /*
           1) Change simple graph so that you need not remove self-loops later      
           2) Edge type <Object> to <CyEdge>, and edge factory fails
@@ -43,12 +44,12 @@ public class IsoThread extends Thread{
             if(e.getSource().equals(e.getTarget())){
                 continue; // removing self-loops
             }
-            g1.addEdge(e.getSource(), e.getTarget());
+            g1.addEdge(e.getSource(), e.getTarget(),e);
         }
         
         UndirectedGraph<CyNode, CyEdge> g2 = new SimpleGraph<CyNode, CyEdge>(CyEdge.class);
-        List<CyNode> nodeList2 = network1.getNodeList();
-        List<CyEdge> edgeList2 = network1.getEdgeList();
+        List<CyNode> nodeList2 = network2.getNodeList();
+        List<CyEdge> edgeList2 = network2.getEdgeList();
         for(CyNode n : nodeList2){
             g2.addVertex(n);
         }
@@ -56,11 +57,11 @@ public class IsoThread extends Thread{
             if(e.getSource().equals(e.getTarget())){
                 continue; // removing self-loops
             }
-            g2.addEdge(e.getSource(), e.getTarget());
+            g2.addEdge(e.getSource(), e.getTarget(), e);
         }
         
         //GraphIsomorphismInspector iso = AdaptiveIsomorphismInspectorFactory.createIsomorphismInspector(g1, g2, null, null);// dafault comparators
-        GraphIsomorphismInspector iso = AdaptiveIsomorphismInspectorFactory.createIsomorphismInspector(g1, g2, new NodeLabelEquivalenceComparator(), new EdgeLabelEquivalenceComparator());
+        GraphIsomorphismInspector iso = AdaptiveIsomorphismInspectorFactory.createIsomorphismInspector(g1, g2, null, null);
         boolean isoResult = iso.isIsomorphic();
   
         if (isoResult) {
